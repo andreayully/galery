b409c4b12cd1b85f7dad8833c72fddf91fe43558
andreayully <yully.guzman@correounivalle.edu.co>
2019-02-04 23:17:19 -0500

    image preview

--- pictures/forms.py
+++ pictures/forms.py
@@ -1,29 +1,10 @@
 from django import forms
 from django.forms import ModelForm
-from pictures.models import RelatedPhoto
+from pictures.models import Photo
 
-
-# class PhotoForm(ModelForm):
-#     class Meta:
-#         model=Photo
-#         exclude=['date_upload']
-#         fields=['title', 'image']
 
 class PhotoForm(ModelForm):
-    image = forms.ModelChoiceField(queryset= RelatedPhoto.objects.all(), required=False)
-
     class Meta:
+        model=Photo
         exclude=['date_upload']
-        fields=['title']
-
-    def __init__(self, *args, **kwargs):
-        super(PhotoForm, self).__init__(*args, **kwargs)
-        if self.instance.pk:
-            self.fields['image'],initial= models.RelatedPhoto.objects.get(photopost__photoinfo=self.instance).values_list('id', flat=True)
-
-    def save(self, commit=True):
-        super(PhotoForm, self).save(commit)
-        image = self.cleaned_data.pop('image', None)
-        if image:
-            self.instance.photopost.delete()
-            models.PhotoPost.objects.create(photoInfo=self.instance, image=image,)
+        fields=['title', 'image']
--- pictures/models.py
+++ pictures/models.py
@@ -1,21 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class RelatedPhoto(models.Model):
+class Photo(models.Model):
+    title = models.CharField(max_length=30)
+    date_upload = models.DateTimeField(auto_now_add=True, blank=True)
     image= models.FileField(upload_to='uploads/')
 
     def __str__(self):
-        return self.image.url
-
-
-class PhotoInfo(models.Model):
-    title = models.CharField(max_length=30)
-    date_upload = models.DateTimeField(auto_now_add=True, blank=True)
-    #image= models.FileField(upload_to='uploads/')
-
-    def __str__(self):
         return self.title
-
-class PhotoPost(models.Model):
-    image = models.ForeignKey(RelatedPhoto)
-    photoInfo= models.ForeignKey(PhotoInfo)
